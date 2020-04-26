d1f3bcba2983f7b2788016d42c5481abb706c578
gigitac <58675739+gigitac@users.noreply.github.com>
2020-04-25 21:31:54 -0600

    Main Commit
    Main commit, including all of the program code equipped.

---
+++ discordMDT/src/main/java/main.java
@@ -0,0 +1,40 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+public class main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Scanner inputSystem = new Scanner(System.in);
+        System.out.println("Discord Mobile Data Terminal");
+        System.out.println("Developed by gigitac#8656");
+        System.out.println("You need a bot token to use this.");
+
+        // ID Info
+        System.out.println("Input bot token.");
+        String bottoken = inputSystem.nextLine();
+        System.out.println("Input userID");
+        String userid = inputSystem.nextLine();
+        DiscordApi api = new DiscordApiBuilder().setToken(bottoken).login().join();
+
+        // Strings
+        String username = ("Username: " + api.getUserById(userid).get().getDiscriminatedName());
+        String creationDate = ("Date of Creation: " + api.getUserById(userid).get().getCreationTimestamp());
+        String userStatus = ("User Status: " + api.getUserById(userid).get().getStatus());
+        String isBot = ("null");
+        if (api.getUserById(userid).get().isBot()) {
+            isBot = ("isBot: TRUE");
+        } else {
+            isBot = ("isBot: FALSE");
+        }
+
+        System.out.println(username);
+        System.out.println(creationDate);
+        System.out.println(userStatus);
+        System.out.println(isBot);
+
+    }
+}
+
